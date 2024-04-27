53bfc21265ea36669199c402715e037346626bbd
Torsten Paul <Torsten.Paul@gmx.de>
2024-04-28 00:49:10 +0200

    Add font preview and more filtering.

--- src/gui/MainWindow.cc
+++ src/gui/MainWindow.cc
@@ -838,7 +838,6 @@ void MainWindow::onTranslateEvent(InputEventTranslate *event)
   } else {
     qglview->translate(zoomFactor * event->x, event->y, zoomFactor * event->z, event->relative, false);
   }
-
 }
 
 void MainWindow::onRotateEvent(InputEventRotate *event)
@@ -3338,7 +3337,6 @@ void MainWindow::hideAnimate()
 
 void MainWindow::showFontList()
 {
-  std::cout << "MainWindow::showFontList()\n";
   windowActionHideFontList->setChecked(false);
   fontListWidget->update_font_list();
   fontListDock->show();
